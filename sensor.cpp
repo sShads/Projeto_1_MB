ad1c5c76cd2fb6b756520e29c160250e4ce3f9a5
sShads <149434625+sShads@users.noreply.github.com>
2024-10-24 02:34:44 +0000

    FIm comentado(FINAL)

--- sensor.cpp
+++ sensor.cpp
@@ -7,21 +7,21 @@ class Sensor: public Componente{
 
     private:
 
-        int t;
+
 
     public:
         Sensor(){}
         Sensor(string nome):
         Componente(nome){}
 
-        void atualizarT(){
+        void atualizarT(){//gera um novo valor para temperatura
              valor=rand()%20+15;
              //return valor;
         };
-        void atualizarU(){
+        void atualizarU(){//gera um novo valor para umidade
             valor=rand()%101;
         };
-        void atualizarL(){
+        void atualizarL(){//gera um novo valor para luminosidade
             valor=rand()%201+50;
         };
 };
